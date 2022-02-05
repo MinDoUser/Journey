cb7143ac689ddd5013ec5dd48a59a17908fc7cd4
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-02-05 14:17:01 +0100

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -8,29 +8,42 @@ import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
-
+// Well, Mindustry itself does not accept any other main class. It just throws a 'NoClassFoundException'
 public class ExampleJavaMod extends Mod{
 
     public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
+                BaseDialog dialog = new BaseDialog("Welcome!");
                 dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
     }
+    public void welcomeDialog(){
+   		BaseDialog dialog = new BaseDialog("Welcome");
+		dialog.closeOnBack();
+		dialog.cont.pane(inner -> {
+			inner.pane(table -> {
+				table.pane(p -> {
+					p.add("[green]<< WELCOME >>", Styles.techLabel).row();
+				}).fillY().growX().row();
+				table.add("").row();
+			}).growX().center().row();
+			
+			inner.table(table -> {
+				table.table(t -> {
+          t.add("Welcome to[stat]Journey[white], a Mod by [cyan]lol02[].\n\nMy advice is to not use OP Mods when playing campaign. \n This will reduce your game experience.").row();
+					t.button("Let's go!", dialog::hide).size(150f, 100f);
+				}).grow();
+			}).fill();
+		}).grow();
+	dialog.show();
+   }
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
     }
 
 }
